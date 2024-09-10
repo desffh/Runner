610eaa8f1d206ca41dd3b1c173366d624564f055
chichi <yangju058@gmail.com>
2024-09-10 19:48:29 +0900

    (2024.9.10) 게임 리소스 제작 및 배치

---
+++ Endless Runner/Assets/Scripts/Runner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum RoadLine
+{
+    LEFT = -1,
+    MIDDLE = 0,
+    RIGHT = 1
+}
+
+public class Runner : MonoBehaviour
+{
+    [SerializeField] RoadLine roadline;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
