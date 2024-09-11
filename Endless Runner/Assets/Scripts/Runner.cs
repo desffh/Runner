028e33a159f61722dfc421b9937f4f5d193985c6
chichi <yangju058@gmail.com>
2024-09-11 19:46:18 +0900

    (2024.9.11) 캐릭터 이동 시스템

--- Endless Runner/Assets/Scripts/Runner.cs
+++ Endless Runner/Assets/Scripts/Runner.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 public enum RoadLine
@@ -13,16 +15,56 @@ public enum RoadLine
 public class Runner : MonoBehaviour
 {
     [SerializeField] RoadLine roadline;
+    [SerializeField] Rigidbody rigidBody;
 
+    [SerializeField] float positionX = 2.5f;
+    [SerializeField] float speed = 25.0f;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        roadline = RoadLine.MIDDLE;
     }
 
+    void OnKeyUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (roadline != RoadLine.LEFT)
+            {
+                roadline--;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (roadline != RoadLine.RIGHT)
+            {
+                roadline++;
+            }
+        }
+    }
+
+    private void Move()
+    {
+        rigidBody.position = Vector3.Lerp
+            (
+            rigidBody.position,
+            new Vector3((int)roadline * positionX, 0, 0),
+            speed * Time.fixedDeltaTime
+            );
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        OnKeyUpdate();
     }
 }
