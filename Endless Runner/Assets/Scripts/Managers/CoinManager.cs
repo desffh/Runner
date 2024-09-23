4e46d75683027e9ab232b15ce1f3264b2e5f1774
chichi <yangju058@gmail.com>
2024-09-23 19:46:10 +0900

    (2024.9.23) 코인 관리자 시스템

---
+++ Endless Runner/Assets/Scripts/Managers/CoinManager.cs
@@ -0,0 +1,32 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+
+    [SerializeField] int createCount = 16;
+
+    [SerializeField] float offset = 2.5f;
+    private void Awake()
+    {
+        Create();
+    }
+    public void Create()
+    {
+        for (int i = 0; i < createCount; i++)
+        {
+            GameObject instance = Instantiate(prefab);
+            instance.transform.SetParent(gameObject.transform); // 자식오브젝트로 생성
+            instance.transform.localPosition = 
+                new Vector3(0f, prefab.transform.position.y, offset * i);
+        }
+    }
+    public void InitializePosition()
+    {
+
+    }
+}
