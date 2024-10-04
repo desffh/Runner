a3ae2cd624c2611b89ee1e7fa43b71c826da58fa
chichi <yangju058@gmail.com>
2024-10-04 19:45:57 +0900

    (2024.10.4) 마우스 관리자 시스템

--- Endless Runner/Assets/Scripts/Managers/ObstacleManager.cs
+++ Endless Runner/Assets/Scripts/Managers/ObstacleManager.cs
@@ -6,6 +6,7 @@ public class ObstacleManager : MonoBehaviour
 {
     [SerializeField] int createCount = 5;
 
+    [SerializeField] int random;
     [SerializeField] List<GameObject> obstacleList;
 
     // Start is called before the first frame update
@@ -14,6 +15,8 @@ public class ObstacleManager : MonoBehaviour
         obstacleList.Capacity = 20;
 
         Create();
+
+        StartCoroutine(ActiveObstacle());
     }
 
     public void Create()
@@ -39,12 +42,37 @@ public class ObstacleManager : MonoBehaviour
         }
         return true;
     }
-
     public IEnumerator ActiveObstacle()
     {
-        while(true)
+        while (true)
         {
             yield return CoroutineCache.WaitForSecond(2.5f);
+
+            random = Random.Range(0, obstacleList.Count);
+
+            // 현재 게임 오브젝트가 활성화되어 있는 지 확인합니다.
+            while (obstacleList[random].activeSelf == true)
+            {
+                // 현재 리스트에 있는 모든 게임 오브젝트가 활성화되어 있는 지 확인합니다.
+                if (ExamineActive())
+                {
+                    // 모든 게임 오브젝트가 활성화되어 있다면 게임 오브젝트를 
+                    // 새로 생성한 다음 obstacleList에 넣어줍니다.
+                    GameObject clone = ResourcesManager.Instance.Instantiate("Coin", gameObject.transform);
+
+                    clone.SetActive(false);
+
+                    obstacleList.Add(clone);
+                }
+
+                // 현재 인덱스에 있는 게임 오브젝트가 활성화되어 있으면
+                // random 변수의 값을 +1 해서 다시 검색합니다.
+                random = (random + 1) % obstacleList.Count;
+            }
+
+            // 랜덤으로 설정된 Obstacle 오브젝트를 활성화합니다.
+            obstacleList[random].SetActive(true);
         }
     }
+
 }
